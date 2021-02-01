e5501c922888781d3c90bae535c0ef4e257063dc
LariDG <larisa.d-g@hotmail.co.uk>
2021-02-01 12:35:11 +0000

    changed to having a true and reco file

--- ml_watchman.C
+++ ml_watchman.C
@@ -22,13 +22,16 @@ using namespace std;
 
 int ml_watchman() {
     
-    TFile * input = new TFile("","READONLY");
+    TFile * input = new TFile("b_elect_flat_3_wbls_10000_runs.root","READONLY");
     TTree * regTree = (TTree*) input -> Get("data");
     
     
     
-    ofstream csvfile;
-    csvfile.open ("merged_electrons_fit_wbls_3_baseline.csv");
+    ofstream mc_data;
+    mc_data.open ("elect_flat_3_wbls_10000_runs_mc.csv");
+    
+    ofstream reco_data;
+    reco_data.open ("elect_flat_3_wbls_10000_runs_reco.csv");
 
     
     
@@ -41,16 +44,19 @@ int ml_watchman() {
     double x; regTree->SetBranchAddress("x", &x);
     double y; regTree->SetBranchAddress("y", &y);
     double z; regTree->SetBranchAddress("z", &z);
-    double n100_prev; regTree->SetBranchAddress("n100_prev", &n100_prev);
+    //double n100_prev; regTree->SetBranchAddress("n100_prev", &n100_prev);
     double dxmcx; regTree->SetBranchAddress("dxmcx", &dxmcx);
     double dymcy; regTree->SetBranchAddress("dymcy", &dymcy);
     double dzmcz; regTree->SetBranchAddress("dzmcz", &dzmcz);
     double drmcr; regTree->SetBranchAddress("drmcr", &drmcr);
+    double closestPMT; regTree->SetBranchAddress("closestPMT", &closestPMT);
     double closestPMT_prev; regTree->SetBranchAddress("closestPMT_prev", &closestPMT_prev);
     
     
     
-    csvfile << "gtid " << "mc_energy " << "mcx " << "mcy " << "mcz " << "pe " << "x " << "y " << "z " << "n_100 " << "dxmcx " << "dymcy " << "dzmcz " << "drmcr " << "closestPMT_prev " << "reco_wall_r " << "reco_wall_z " << "true_wall_r " << "true_wall_z " << "\n";
+    mc_data << "gtid," << "mcx," << "mcy," << "mcz," << "mc_energy," << "closestPMT," << "true_wall_r," << "true_wall_z," << "\n";
+    
+    reco_data << "gtid," << "x," << "y," << "z," << "pe," << "closestPMT_prev," << "reco_wall_r," << "reco_wall_z," << "/n";
     
     for (Long64_t ievt=0; ievt<regTree->GetEntries();ievt++) {
         regTree -> GetEntry(ievt);
@@ -62,21 +68,25 @@ int ml_watchman() {
         }
         
         double reco_vtx_r = x*x + y*y;
-        double reco_wall_r = 10000-TMath::Sqrt(recoVtxR);
+        double reco_wall_r = 10000-TMath::Sqrt(reco_vtx_r);
         double reco_wall_z = 10000-TMath::Abs(z);
         
-        double true_vtx_r = mcx+mcx + mcy*mcy;
-        double true_wall_r = 10000-TMath::Sqrt(trueVtxR);
+        double true_vtx_r = mcx*mcx + mcy*mcy;
+        double true_wall_r = 10000-TMath::Sqrt(true_vtx_r);
         double true_wall_z = 10000-TMath::Abs(mcz);
         
         
+        mc_data << gtid << "," << mcx << "," << mcy << "," << mcz << "," << mc_energy << "," << closestPMT << "," << true_wall_r << "," << true_wall_z << "\n";
         
-        csvfile << gtid << " " << mc_energy << " " << mcx << " " << mcy << " " << mcz << " " << pe << " " << x << " " << y << " " << z << " " << n100_prev << " " << dxmcx << " " << dymcy << " " << dzmcz << " " << drmcr << " " << closestPMT_prev << " " << reco_wall_r << " " << reco_wall_z << " " << true_wall_r << " " << true_wall_z << "\n" ;
+        reco_data << gtid << "," << x << "," << y << "," << z << "," << pe << "," << closestPMT_prev << "," << reco_wall_r << "," << reco_wall_z << "\n";
+        
       
 
     };
 
-    csvfile.close();
+    mc_data.close();
+    reco_data.close();
+    
     return 0;
 }
 
--- ml_watchman.py
+++ ml_watchman.py
@@ -9,6 +9,7 @@ import sys
 import matplotlib
 import matplotlib.pyplot as plt
 import random
+import pandas as pd
 
 from sklearn import datasets
 from sklearn import model_selection
@@ -20,6 +21,12 @@ from sklearn.metrics import mean_squared_error
 
 
 infile = "file.csv"
+file = open(str(infile))
+data = pd.read_csv(file)
+
+data = data[0].str.split(',',expand=True)
+
+
 
 seed = 150
 np.random.seed(seed)
